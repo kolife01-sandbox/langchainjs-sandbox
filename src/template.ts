9079f4d1beb325c47cd56bdd3f02e6a383a8fb0d
Kohei Tanaka <kohei69@gmail.com>
2023-06-04 11:53:11 +0900

    add template

---
+++ src/template.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import { PromptTemplate } from 'langchain'
+
+const main = async () => {
+  const template = 'What is a good name for a company that makes {product}?'
+  const prompt = new PromptTemplate({
+    template: template,
+    inputVariables: ['product'],
+  })
+
+  const res = await prompt.format({ product: 'socks' })
+  console.log(res)
+}
+
+main()
