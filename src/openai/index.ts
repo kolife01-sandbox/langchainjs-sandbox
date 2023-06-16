dad7952c8aeccb834b79aeb376216f2aa301a533
Kohei Tanaka <kohei69@gmail.com>
2023-06-16 21:23:30 +0900

    Refactor openai index.ts file

--- src/openai/index.ts
+++ src/openai/index.ts
@@ -1,18 +1,17 @@
 import 'dotenv/config'
-import { Configuration, OpenAIApi  } from 'openai'
+import { Configuration, OpenAIApi } from 'openai'
 
 const main = async () => {
-
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+  })
+  const openai = new OpenAIApi(configuration)
 
   const chatCompletion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: "Hello world"}],
-  });
-  console.log(chatCompletion.data.choices[0].message);
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: 'Hello world' }],
+  })
+  console.log(chatCompletion.data.choices[0].message)
 }
 
 main()
